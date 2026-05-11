bbc4ae3209ed93b29d8946c517104c1539077975
zaklykh <habibizakly47@gmail.com>
2026-05-11 07:49:10 +0700

    update

--- Modul 10/kelas/buah.php
+++ Modul 10/kelas/buah.php
@@ -1,12 +1,15 @@
 <?php
-require_once ('Mahasiswa.php');
+class buah
+{
+    public $nama;
+    protected $warna;
+    private $berat;
+    
+}
 
-$mhs1 = new mahasiswa("Habibi Zakly Khairullah");
-$mhs1->setNIM("253307048");
-$mhs1->setKelas("2B");
+$mango = new buah();
+$mango->nama = 'Mango'; // OK
+// $mango->warna = 'Yellow'; // ERROR: Access to protected property
+// $mango->buah = '300'; // ERROR: Variable not found
 
-// tampilkan nama nim dan kelas dari $mhs1
-echo "Nama: " . $mhs1->getNama() . "<br>";
-echo "NIM: " . $mhs1->getNim() . "<br>";
-echo "Kelas: " . $mhs1->getKelas() . "<br>";
 ?>
